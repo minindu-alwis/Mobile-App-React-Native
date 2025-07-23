a409ee0d547f6db472ca0387229fec87d7b4d4d0
minindu-alwis <minindualwis@gmail.com>
2025-07-23 14:08:02 +0530

    Refactor index component and update styling; add onboarding screen and extend Tailwind theme colors.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
-export default function App() {
+export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-blue-500">Hello, Tailwind!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-200 font-bold">Huttoooooooo</Text>
+      <Link href="/onboarding">Minndu Alwis</Link>
     </View>
   );
 }
